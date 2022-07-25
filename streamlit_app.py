288387c24dfff34e9532127c3a2a6206c9316da5
Shehim Mohamed <57039816+shahim22@users.noreply.github.com>
2022-07-25 10:51:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 streamlit.title('All good here..')
 streamlit.title('A Healthy Breakfast is the one which you like.')
 streamlit.title('And of course eat healthy.')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
