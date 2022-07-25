1341a51d4a845935c8511728f02b05193280938f
Shehim Mohamed <57039816+shahim22@users.noreply.github.com>
2022-07-25 10:35:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,4 @@ import streamlit
 streamlit.title('All good here..')
 streamlit.title('A Healthy Breakfast is the one which you like.')
 streamlit.title('And of course eat healthy.')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
