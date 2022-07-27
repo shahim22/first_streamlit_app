605601efc98a6625ef9dc1b437e567033668ff39
Shehim Mohamed <57039816+shahim22@users.noreply.github.com>
2022-07-27 09:53:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 def get_fruityvice_data(this_fruit_choice):
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-return fruityvice_normalized    
+     return fruityvice_normalized    
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
