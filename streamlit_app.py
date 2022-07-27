ddaac26a3186cf326b957be2bee2b6726700895b
Shehim Mohamed <57039816+shahim22@users.noreply.github.com>
2022-07-27 09:52:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,11 @@ streamlit.title('And of course eat healthy.')
 streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
 #import pandas
 
+#create repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+return fruityvice_normalized    
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -25,9 +30,8 @@ try:
      if not fruit_choice:
            streamlit.error("Please select a fruit to get information.")
      else:   
-           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-           fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-           streamlit.dataframe(fruityvice_normalized)
+           back_from_function = get_fruityvice_data(fruit_choice)
+           streamlit.dataframe(back_from_function)
 
 except URLError as e:
      streamlit.error()
